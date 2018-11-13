2a462a4d2bf93e43af53102cd46a78f1802b5c6b
miguel <mikepruebas23@gmail.com>
2018-11-12 20:33:05 -0600

    menu

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  
+  /* CLick BTN (Open and Close) */
+	$('.button-nav--toggle').on('click', function(e) {
+      e.preventDefault();
+      $('body').toggleClass('is-showNavMob');
+	});
+  
+});
