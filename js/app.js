001197771c1043ad801b7644ea8092706b91513d
Miguel Angel Armenta Acosta <mikepruebas23@gmail.com>
2018-11-27 17:02:07 -0700

    agregando face y youtube

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
-var galletas = 0;
+var dinero = 0;
 
 //arreglo,cursos,abuelitas,galelta tenemos
 var inventario = [0, 0, 0];
 
 //llevar la cuenta de cuantos producen
-var galletasProduce = [1, 2, 4];
+var dineroProduce = [1, 2, 4];
 
-//cuantas galletas nos va a costar
+//cuantas dinero nos va a costar
 var precioProducto = [10, 200, 400];
 
-//funcion click incrementar galletas
+//funcion click incrementar dinero
 function clic() {
-    galletas++;
+    dinero++;
 }
 
 //funcion para comprar cosas
 function comprar(objeto) {
 
     //comrpar con dinero suficiente
-    if (galletas >= precioProducto[objeto]) {
+    if (dinero >= precioProducto[objeto]) {
         inventario[objeto]++;
-        galletas -= precioProducto[objeto];
+        dinero -= precioProducto[objeto];
+        precioProducto[objeto] = precioProducto[objeto] * 2;
         var circulos = document.getElementById("circulo");
         circulos.classList.add("show");
 
     } else {
-        console.log("No tienes suficiente dinero");
-        myFunction();
+        mostrarNoDinero();
     }
 
 }
@@ -34,12 +34,12 @@ function comprar(objeto) {
 //producir automatico
 function producir() {
     for (contador = 0; contador < inventario.length; contador++) {
-        galletas += inventario[contador] * galletasProduce[contador];
+        dinero += inventario[contador] * dineroProduce[contador];
     }
 }
-//actualizar el numero de galletas que hay
+//actualizar el numero de dinero que hay
 function render() {
-    document.getElementById("contador").innerHTML = galletas;
+    document.getElementById("contador").innerHTML = dinero;
     document.getElementById("inventario").innerHTML =
         `<div>Cursores: ${inventario[0]}</div>
         <div>Cursores: ${inventario[1]}</div>
@@ -52,18 +52,17 @@ var FPS = 1;
 setInterval(function() {
     //se clickea automaticamente
     producir();
-    //actualice la cantidad de galletas que hay en el momento
+    //actualice la cantidad de dinero que hay en el momento
     render();
 }, 1000 / FPS);
 
 // Mostrar SnackBar
-function myFunction() {
+function mostrarNoDinero() {
     var x = document.getElementById("NoDinero1");
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 function crearCirculo(objeto) {
     var circulos = document.getElementById("circulo");
-    // circulos.className = "show";
     circulos.classList.add("show");
 }
